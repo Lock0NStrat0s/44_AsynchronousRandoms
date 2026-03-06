12a6a02ac3f24cc603416ca235273d1eaa931128
Mohammad Jabir <mjabir94@proton.me>
2026-03-06 14:54:34 -0700

    challenge complete.

--- AsynchronousRandoms/Program.cs
+++ AsynchronousRandoms/Program.cs
@@ -2,8 +2,58 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string? input;
+        do
+        {
+            Console.Write("Enter Word to start (blank to exit): ");
+            input = Console.ReadLine();
+            HandleMethod(input);
+
+        } while (input != "");
+    }
+
+    static async Task HandleMethod(string? input)
+    {
+        DateTime start = DateTime.Now;
+
+        int attempts = await RandomlyRecreateAsync(input);
+        Console.WriteLine($"\n\n\nWord: {input} has {attempts} attempts");
+
+
+        DateTime end = DateTime.Now;
+        TimeSpan timeElapsed = start - end;
+        Console.WriteLine($"Total time: {timeElapsed.TotalSeconds}");
+    }
+
+    static int RandomlyRecreate(string word)
+    {
+        Random random = new Random();
+        int attempts = 0;
+        string newWord;
+
+        do
+        {
+            newWord = "";
+
+            while (newWord.Length < word.Length)
+            {
+                char temp = (char)('a' + random.Next(26));
+                newWord += temp;
+            }
+
+            //Console.WriteLine(newWord);
+
+            attempts++;
+        } while (newWord != word);
+
+
+        return attempts;
+    }
+
+    static Task<int> RandomlyRecreateAsync(string word)
+    {
+        return Task.Run(() => RandomlyRecreate(word));
     }
 }
